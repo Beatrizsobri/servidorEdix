c6b14c0f9d97440a103f5822ab188b985441ac75
Raul-Docs <raul.cal.bar@gmail.com>
2023-03-07 20:29:27 +0100

    proyecto subido

--- zapateria/zapateria/src/main/java/com/ite/zapateria/repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ite.zapateria.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ite.zapateria.modelo.entities.Pedido;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
-
-}
--- zapateria/zapateria/src/main/java/com/ite/zapateria/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ite.zapateria.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ite.zapateria.modelo.entities.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
-
-}
