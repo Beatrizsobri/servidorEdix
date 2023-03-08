4f04131e72f6905aec3ba7ff97609491a39e2406
Jesús Raboso Cruz <99287469+jesusrc93@users.noreply.github.com>
2023-03-08 02:36:41 +0100

    Borro la desactualizada

--- zapateria/src/main/java/com/ite/zapateria/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ite.zapateria.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ite.zapateria.modelo.entities.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Integer>{
-
-}
